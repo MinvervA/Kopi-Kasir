a7d2e2d4dbfb8338902f9f859180237cf6f44940
MinvervA <andrean923@gmail.com>
2025-12-25 06:04:04 +0700

    setup frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="">
+        <p className="text-4xl text-red-500">text</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
